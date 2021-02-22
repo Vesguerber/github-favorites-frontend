b21ef7f54cfb3f99da6ff9daa5cefdc72d6a06f4
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2021-02-22 15:35:31 -0300

    search form front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RepoListComponent } from './repo-list/repo-list.component';
 import { RepoCardComponent } from './repo-list/repo-card/repo-card.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { RepoCardComponent } from './repo-list/repo-card/repo-card.component';
     MainPageComponent,
     RepoListComponent,
     RepoCardComponent,
+    SearchFormComponent,
   ],
   imports: [
     BrowserModule,
